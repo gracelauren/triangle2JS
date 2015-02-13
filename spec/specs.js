cddd19c1ec44e97e9490876fc28335597004e88a
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-13 09:59:35 -0800

    Add spec for equilateral triangles

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangle', function() {
   it('it returns false if not a triangle', function() {
     expect(triangle(2,2,8)).to.equal('NOT A TRIANGLE!');
   });
+
+  it('it returns equilateral for equilateral triangles', function() {
+    expect(triangle(3,3,3)).to.equal('Equilateral');
+  });
 });
