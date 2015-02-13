001cd7a6209d284e11f3ccb90ed9d3020d472a69
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-13 09:51:09 -0800

    Add script for false triangles

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var triangle = function(sideA, sideB, sideC) {
+  if (sideA + sideB <= sideC) {
+    return "NOT A TRIANGLE!"
+  } else if (sideA + sideC <= sideB) {
+    return "NOT A TRIANGLE!"
+  } else if (sideB + sideC <= sideA) {
+    return "NOT A TRIANGLE!"
+  } else {
+    
+  }
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('triangle', function() {
   it('it returns false if not a triangle', function() {
-    expect(triangle(2,2,8)).to.equal('Not a triangle!');
+    expect(triangle(2,2,8)).to.equal('NOT A TRIANGLE!');
   });
 });
