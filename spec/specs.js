b34e8c6e3f5d0693ec87766b8eb8130aa4e0618b
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-13 10:03:48 -0800

    Add spec for scalene triangles

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('triangle', function() {
-  it('it returns false if not a triangle', function() {
+  it('returns false if not a triangle', function() {
     expect(triangle(2,2,8)).to.equal('NOT A TRIANGLE!');
   });
 
-  it('it returns equilateral for equilateral triangles', function() {
+  it('returns equilateral for equilateral triangles', function() {
     expect(triangle(3,3,3)).to.equal('Equilateral');
+  });
+
+  it('returns scalene for scalene triangles', function() {
+    expect(triangle(6,12,14)).to.equal('Scalene');
   });
 });
